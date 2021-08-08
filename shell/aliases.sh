f02636210a7f9a13433a6844a488742bd2726adc
Mikel Goig <m.goig@docline.com>
2021-08-08 14:26:46 +0200

    style aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,21 +9,21 @@ alias ll='ls -l'
 alias la='ls -la'
 alias ~='cd ~'
 alias dotfiles='cd $DOTFILES_PATH'
-alias code="cd $CODE_PATH"
-alias docline="cd $CODE_PATH/docline"
+alias code='cd $CODE_PATH'
+alias docline='cd $CODE_PATH/docline'
 
 # Git
-alias gaa="git add -A"
+alias gaa='git add -A'
 alias gc='$DOTLY_PATH/bin/dot git commit'
-alias gca="git add --all && git commit --amend --no-edit"
-alias gco="git checkout"
+alias gca='git add --all && git commit --amend --no-edit'
+alias gco='git checkout'
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status"
-alias gf="git fetch --all -p"
-alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull --rebase --autostash"
-alias gb="git branch"
+alias gs='git status'
+alias gf='git fetch --all -p'
+alias gps='git push'
+alias gpsf='git push --force'
+alias gpl='git pull --rebase --autostash'
+alias gb='git branch'
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
 # Utils
@@ -35,4 +35,4 @@ alias p.='(phpstorm $PWD &>/dev/null &)'
 alias up='dot package update_all'
 
 # Docker
-alias dcon="dot docker connect"
+alias dcon='dot docker connect'
