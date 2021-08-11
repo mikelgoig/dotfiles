37ee1623d26db5d695e989f9d1e5b222ebb57560
Mikel Goig <m.goig@docline.com>
2021-08-11 10:50:05 +0200

    style: aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,19 +1,12 @@
 # Enable aliases to be sudo’ed
 alias sudo="sudo "
 
-# Navigation
-alias cl="clear"
-alias ..="cd .."
-alias ...="cd ../.."
-alias ll="ls -l"
-alias la="ls -la"
+# Docker
 
-# Favorite dirs
-alias dotfiles="cd $DOTFILES_PATH"
-alias code="cd $CODE_PATH"
-alias docline="cd $CODE_PATH/docline"
+alias dcon="$DOTLY_PATH/bin/dot docker connect"
 
 # Git
+
 alias gaa="git add -A"
 alias gc="$DOTLY_PATH/bin/dot git commit"
 alias gca="git add --all && git commit --amend --no-edit"
@@ -27,13 +20,25 @@ alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
 alias gl="$DOTLY_PATH/bin/dot git pretty-log"
 
+# Navigation
+
+alias cl="clear"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ll="ls -l"
+alias la="ls -la"
+
+alias dotfiles="cd $DOTFILES_PATH"
+alias code="cd $CODE_PATH"
+alias docline="cd $CODE_PATH/docline"
+
 # Utils
+
 alias k="kill -9"
+
 alias i.="(idea . &>/dev/null &)"
 alias c.="(vscode . &>/dev/null &)"
 alias o.="open ."
 alias p.="(phpstorm . &>/dev/null &)"
-alias up="$DOTLY_PATH/bin/dot package update_all"
 
-# Docker
-alias dcon="$DOTLY_PATH/bin/dot docker connect"
+alias up="$DOTLY_PATH/bin/dot package update_all"
